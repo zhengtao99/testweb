2120e2aaa49c9beb6819c9c28b21517fb70cee0b
Zheng Tao <zhengtaolim@gmail.com>
2020-10-06 10:32:27 +0800

    aa

--- js/SSDC_BookingAvailability.js
+++ js/SSDC_BookingAvailability.js
@@ -1,5 +1,6 @@
 /* https://www.ssdcl.com.sg/User/Booking/AddBooking */
 let days_ahead = 3;
+let weeks_ahead = 12;
 let favourite_time = "6:10 PM";
 //let days = ["Monday","Tuesday","Wednesday","Thursday","Friday"];
 let days = ["Friday"];
@@ -23,18 +24,35 @@ setTimeout(function(){
 			}
 		}
 	});
-	verify();
+	SetDate();
 },4000);
+
 
 function SetDate(){
 	var nextDayDate = new Date();
-	nextDayDate.setDate(nextDayDate.getDate() + days_ahead);
+	nextDayDate.setDate(nextDayDate.getDate() + days_ahead + GetWeek());
 	var dateString = nextDayDate.toDateString();
 
 	var dateArr = dateString.split(" ");
 	var newDate = dateArr[2] + " " + dateArr[1] + " " + dateArr[3];
 	changeDate(newDate);
 }
+
+function GetWeek(){
+	var week = 0;
+	if(sessionStorage.getItem("weeks_ahead") != null)
+	{
+		week = parseInt(sessionStorage.getItem("weeks_ahead"));
+		week += 1;		
+	}
+	if(week > weeks_ahead)
+	{
+		week = 0;
+	}
+	sessionStorage.setItem("weeks_ahead", week);
+	return week * 7;
+}
+
 function CheckPending(){
 	if($(".blink_me").first().text().includes("You have 1 item pending payment"))
 	{
